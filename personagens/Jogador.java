1bb983a51c8a3313a20d5bb4a2b88523b980ba43
Ingrid Falchi <109180449+ingridfalchii@users.noreply.github.com>
2023-11-29 23:35:49 -0300

    Classe Jogador

--- personagens/Jogador.java
+++ personagens/Jogador.java
@@ -1,5 +1,15 @@
-package personagens;
+//package personagens;
 
 public class Jogador {
-  
+
+    private String nome;
+
+    public Jogador(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
 }
