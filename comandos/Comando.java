0d8224d43c4c2b7b314de614f9727adb9fb05704
Ingrid Falchi <109180449+ingridfalchii@users.noreply.github.com>
2023-11-29 23:20:34 -0300

    Add files via upload

--- comandos/Comando.java
+++ comandos/Comando.java
@@ -1,5 +1,49 @@
-package comandos;
+//package comandos;
 
 public class Comando {
-  
+    // primeira palavra que representa o comando
+    private String palavraDeComando;
+    // segunda palavra que representa um complemento do comando
+    private String segundaPalavra;
+
+    /**
+     * Cria um objeto comando. Primeira e segunda palavra devem ser  fornecidas, mas 
+     * qualquer uma (ou ambas) podem ser null.
+     * @param primeiraPalavra A primeira palavra do comando. Null se o comando não foi reconhecido.
+     * @param segundaPalavra A segunda palavra do comando.
+     */
+    public Comando(String primeiraPalavra, String segundaPalavra) {
+        palavraDeComando = primeiraPalavra;
+        this.segundaPalavra = segundaPalavra;
+    }
+
+    /**
+     * Retorna a palavra de comando (a primeira palavra) deste comando.
+     * Se o comando não foi entendido, o resultado eh null.
+     * @return A palavra de comando.
+     */
+    public String getPalavraDeComando() {
+        return palavraDeComando;
+    }
+
+    /**
+     * @return A segunda palavra deste comando. Retorna null se não existe segunda palavra.
+     */
+    public String getSegundaPalavra() {
+        return segundaPalavra;
+    }
+
+    /**
+     * @return true se o comando não foi entendido.
+     */
+    public boolean ehDesconhecido() {
+        return (palavraDeComando == null);
+    }
+
+    /**
+     * @return true se o comando tem uma segunda palavra.
+     */
+    public boolean temSegundaPalavra() {
+        return (segundaPalavra != null);
+    }
 }
