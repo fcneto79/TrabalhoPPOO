0ff3304b61a7bfa8a423855332fd42914fe8158b
Ingrid Falchi <109180449+ingridfalchii@users.noreply.github.com>
2023-11-29 23:28:22 -0300

    Palavras de comando

---
+++ comandos/PalavrasComando.java
@@ -0,0 +1,29 @@
+//package comandos;
+
+public class PalavrasComando {
+    // um vetor constante que guarda todas as palavras de comandos válidas
+    private static final String[] comandosValidos = {
+        "ir", "sair", "ajuda", "observar", "pegar"
+    };
+
+    public static String getComandos(){
+        String comandos = " ";
+        for(String comando: comandosValidos){
+            comandos += comando + " ";
+        }
+        return comandos;
+    }
+
+    /**
+     * Verifica se uma dada String é uma palavra de comando válida. 
+     * @return true se a string dada é um comando valido, false se não é.
+     */
+    public boolean ehComando(String umaString) {
+        for(int i = 0; i < comandosValidos.length; i++) {
+            if(comandosValidos[i].equals(umaString))
+                return true;
+        }
+        // se chegamos aqui, a string não foi encontrada nos comandos.
+        return false;
+    }
+}
