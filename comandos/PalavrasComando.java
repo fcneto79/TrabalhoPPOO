3e62088b663ab9465621cd2fd6ab40d55d2e0610
Ingrid Falchi <109180449+ingridfalchii@users.noreply.github.com>
2023-11-29 23:48:49 -0300

    Update PalavrasComando.java

--- comandos/PalavrasComando.java
+++ comandos/PalavrasComando.java
@@ -3,7 +3,7 @@
 public class PalavrasComando {
     // um vetor constante que guarda todas as palavras de comandos válidas
     private static final String[] comandosValidos = {
-        "ir", "entrar", "sair", "ajuda", "verificar", "pegar"
+        "ir", "sair", "ajuda", "verificar", "pegar"
     };
 
     public static String getComandos(){
